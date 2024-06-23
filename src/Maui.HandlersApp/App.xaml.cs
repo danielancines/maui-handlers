8c8fca8848846c6e00cb7d1a62843dbcd4520d24
Daniel Ancines <daniel.ancines@gmail.com>
2024-06-23 19:43:05 -0300

    First Commit

---
+++ src/Maui.HandlersApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui.HandlersApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/Maui.HandlersApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui.HandlersApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
